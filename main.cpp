c4a0bc7ec88a60ea054d935d32ec955ede3efb16
Chi <worakun_a@elearning.cmu.ac.th>
2021-01-26 16:35:20 +0700

    create merge function for intersection

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int merge_base(int a[], int b[], int n, int m) {
+  int c = 0, i = 0, j = 0;
+  while (i < n && j < m) {
+    if (a[i] < b[j]) ++i;
+    else if (a[i] > b[j]) ++j;
+    else ++c, ++i, ++j;
+  }
+  return c;
+}    
+
 int main() {
   int a[] = {1, 2, 3, 5, 6, 7, 10};
   int b[] = {0, 4, 5, 7, 9, 11, 12};
