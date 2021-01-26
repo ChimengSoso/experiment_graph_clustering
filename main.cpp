60355e58cbecaf0bf4cfdcfc5fb133c10c56d72c
Chi <worakun_a@elearning.cmu.ac.th>
2021-01-26 23:55:56 +0700

    complete testing correctness of intersection algorithm

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<int> bugA;
+vector<pair<int, int>> bugB;
+int DEBUG = 0;
+
 int merge_base(int *a, int *b, int n, int m) {
   int c = 0, i = 0, j = 0;
   while (i < n && j < m) {
     if (a[i] < b[j]) ++i;
     else if (a[i] > b[j]) ++j;
-    else ++c, ++i, ++j;
+    else {
+      if (DEBUG) {
+        bugA.push_back(a[i]);
+      }
+      ++c, ++i, ++j;
+    }
   }
   return c;
 }    
@@ -17,17 +26,22 @@ int bsearch(int val, int *a, int l, int r) {
 
 int intersect(int *a, int *b, int la, int ra, int lb, int rb) {
   if (la > ra || lb > rb) return 0;
+
   int mid_b = (lb + rb + 1) >> 1, c = 0;
   int val = b[mid_b];
   int mid_a = bsearch(val, a, la, ra);
+
   if (mid_b - lb > mid_a - la) {
     c = c + intersect(b, a, lb, mid_b - 1, la, mid_a - 1);
   } else {
     c = c + intersect(a, b, la, mid_a - 1, lb, mid_b - 1);
   }
 
-  if (val == a[mid_a]) {
+  if (mid_a <= ra && val == a[mid_a]) {
     c = c + 1;
+    if (DEBUG) {
+      bugB.push_back({val, mid_a});
+    }
   } else if (val < a[mid_a]) mid_a = mid_a - 1;
 
   if (ra - mid_a > rb - mid_b) {
@@ -35,17 +49,15 @@ int intersect(int *a, int *b, int la, int ra, int lb, int rb) {
   } else {
     c = c + intersect(b, a, mid_b + 1, rb, mid_a + 1, ra);
   }
+
   return c;
 }
 
 int fast_intersect(int *a, int *b, int n, int m) {
   if (n > m) {
     return intersect(a, b, 0, n - 1, 0, m - 1);
-  } else if (m >= n){
+  } else {
     return intersect(b, a, 0, m - 1, 0, n - 1);
-  } else { // the case that not suitable for fast_intersect
-    printf("m and n is equal.");
-    assert(false);
   }
 }
 
@@ -54,9 +66,9 @@ unsigned int randInt(int l, int r) {
   return uniform_int_distribution<unsigned int>(l, r)(rng);
 };
 
-int test_correctness(bool show = false) {
-  int n = (int) randInt(1, 200);
-  int m = (int) randInt(1, 200);
+int test_correctness(int id_test, bool show = false) {
+  int n = (int) randInt(1, 50);
+  int m = (int) randInt(1, 50);
   int *a = new int[n];
   int *b = new int[m];
   for (int i = 0; i < n; ++i) {
@@ -106,6 +118,7 @@ int test_correctness(bool show = false) {
   int fast_cnt = fast_intersect(a, b, n, m);
   if (show) printf("fast intersection count: %d\n", fast_cnt);
   if (merge_cnt != fast_cnt) {
+    printf("id test: %d\n", id_test);
     printf("size array a: %d\n", n);
     printf("Array a:");
     for (int i = 0; i < n; ++i) {
@@ -121,32 +134,21 @@ int test_correctness(bool show = false) {
     printf("\n");
     printf("merge_cnt = %d\n", merge_cnt);
     printf("fast intersect = %d\n", fast_cnt);
-
+    DEBUG = 1;
+    printf("Calculated AGAIN!!! [%d ----- %d]\n",
+       merge_base(a, b, n, m), fast_intersect(a, b, n, m));
+    printf("algor A: "); for (auto e: bugA) printf("%d ", e); printf("\n");
+    printf("algor B: "); for (auto e: bugB) printf("val = %d, idx = %d\n", e.first, e.second); printf("\n");
+    assert(false);
   }
-  assert(merge_cnt == fast_cnt);
   delete a;
   delete b;
 }
-int a[] = {20, 30, 50, 52, 53, 62, 68, 71, 95, 97 };
-int b[] = { 3, 14, 28, 29, 32, 34, 39, 43, 47, 55, 57, 62, 63, 68, 74, 82, 83, 90, 91, 99};
 
 int main() {
-  // int testing = 1000000;
-  // while (testing--) {
-  //   test_correctness();
-  // }
-  // printf("end testing");
-
-  int n = sizeof(a) / sizeof(a[0]);
-  int m = sizeof(b) / sizeof(b[0]);
-  int merge_cnt = merge_base(a, b, n, m);
-  printf("merge = %d\n", merge_cnt);
-  int fast_cnt = fast_intersect(a, b, n, m);
-  if (n != m) {
-    int fast_cnt = fast_intersect(a, b, n, m);
-    printf("fast = %d\n", fast_cnt);
-  } else {
-    printf("fast intersection is stopped. n = %d, m = %d\n", n, m);
+  for (int testing = 1; testing <= 1000000; ++testing) {
+    test_correctness(testing);
   }
+  printf("end testing");
   return 0;
 }
